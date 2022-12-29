fc157a6dda808b302885daaa32cb010e8aa85dc8
Naveed Wani <navid.wani11@gmail.com@gmail.com>
2022-12-29 11:06:40 +0530

    Added configuration and DB

---
+++ web/sites/default/files/php/twig/63a47dcb42b4c_block--footerblock-2.html_Eyx2FMOP0juwfhb7Rle3iNcY9/KZt4QJ6zzeZ7XqrevKc3MQUovW5f66aheE3evdhl9ik.php
@@ -0,0 +1,144 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/myproject/templates/block/block--footerblock-2.html.twig */
+class __TwigTemplate_8459729256ac74f3e71c6e75707482727b05223048d6d9a29bb72dfbef66400a extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 30
+        echo "
+<div";
+        // line 31
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => "footer__container-block"], "method", false, false, true, 31), 31, $this->source), "html", null, true);
+        echo ">
+  <div class='footer__container-logoimg'>";
+        // line 32
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content"] ?? null), "field_footer_image", [], "any", false, false, true, 32), 32, $this->source), "html", null, true);
+        echo "</div>
+  <div class='footer__container-descrip'>";
+        // line 33
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content"] ?? null), "body", [], "any", false, false, true, 33), 33, $this->source), "html", null, true);
+        echo "</div>
+  <div class='footer__container-sociallinks'>";
+        // line 34
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content"] ?? null), "field_footer_media_links", [], "any", false, false, true, 34), 34, $this->source), "html", null, true);
+        echo "</div>
+  <hr class='footer__line'/>
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/myproject/templates/block/block--footerblock-2.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  54 => 34,  50 => 33,  46 => 32,  42 => 31,  39 => 30,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Default theme implementation to display a block.
+ *
+ * Available variables:
+ * - plugin_id: The ID of the block implementation.
+ * - label: The configured label of the block if visible.
+ * - configuration: A list of the block's configuration values.
+ *   - label: The configured label for the block.
+ *   - label_display: The display settings for the label.
+ *   - provider: The module or other provider that provided this block plugin.
+ *   - Block plugin specific settings will also be stored here.
+ * - content: The content of this block.
+ * - attributes: array of HTML attributes populated by modules, intended to
+ *   be added to the main container tag of this template.
+ *   - id: A valid HTML ID and guaranteed unique.
+ * - title_attributes: Same as attributes, except applied to the main title
+ *   tag that appears in the template.
+ * - title_prefix: Additional output populated by modules, intended to be
+ *   displayed in front of the main title tag that appears in the template.
+ * - title_suffix: Additional output populated by modules, intended to be
+ *   displayed after the main title tag that appears in the template.
+ *
+ * @see template_preprocess_block()
+ *
+ * @ingroup themeable
+ */
+#}
+
+<div{{ attributes.addClass('footer__container-block') }}>
+  <div class='footer__container-logoimg'>{{ content.field_footer_image }}</div>
+  <div class='footer__container-descrip'>{{ content.body}}</div>
+  <div class='footer__container-sociallinks'>{{ content.field_footer_media_links }}</div>
+  <hr class='footer__line'/>
+</div>
+", "themes/myproject/templates/block/block--footerblock-2.html.twig", "/var/www/html/web/themes/myproject/templates/block/block--footerblock-2.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 31);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
